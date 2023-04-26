78c6898adca8ee3df30746d4ccbde8af7af6fa24
Samson Akindeinde <sakindeinde@gmail.com>
2023-04-25 17:37:06 -0700

    printf ptoject

--- get_size.c
+++ get_size.c
@@ -1,25 +1,26 @@
 #include "main.h"
 
 /**
- * get_size - Calculates size to cast argument
- * @format: Formatted string.
- * @i: List of arguments.
- * Return: m
+ * get_size - Calculates the size to cast the argument
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ *
+ * Return: Precision.
  */
 int get_size(const char *format, int *i)
 {
-	int om = *i + 1;
-	int m = 0;
+	int curr_i = *i + 1;
+	int size = 0;
 
-	if (format[om] == 'l')
-		m = S_LONG;
-	else if (format[om] == 'h')
-		m = S_SHORT;
+	if (format[curr_i] == 'l')
+		size = S_LONG;
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
 
-	if (m == 0)
-		*i = om - 1;
+	if (size == 0)
+		*i = curr_i - 1;
 	else
-		*i = om;
+		*i = curr_i;
 
-	return (m);
+	return (size);
 }
