90ebb9878959876149021f322720de931ab236f7
Samson Akindeinde <sakindeinde@gmail.com>
2023-04-25 17:12:32 -0700

    printf ptoject

--- get_flags.c
+++ get_flags.c
@@ -2,32 +2,32 @@
 
 /**
  * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
+ * @format: Formatted string to check for flags
+ * @i: Pointer to the current index in the format string
+ * Return: An integer representing the active flags
  */
 int get_flags(const char *format, int *i)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
 	int j, curr_i;
 	int flags = 0;
-	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+	const char FLAG_CHARS[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAG_VALUES[] = {1, 2, 4, 8, 16, 0};
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CH[j])
+		for (j = 0; FLAG_CHARS[j] != '\0'; j++)
+		{
+			if (format[curr_i] == FLAG_CHARS[j])
 			{
-				flags |= FLAGS_ARR[j];
+				flags |= FLAG_VALUES[j];
 				break;
 			}
-
-		if (FLAGS_CH[j] == 0)
+		}
+		if (FLAG_CHARS[j] == 0)
+		{
 			break;
+		}
 	}
-
 	*i = curr_i - 1;
 
 	return (flags);
